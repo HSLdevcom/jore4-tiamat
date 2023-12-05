3eeb13f616bace705dcb6d5330d6e59c5115c708
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-12-05 07:14:45 +0200

    Disable OAuth2 security temporarily
    Disabling security until a proper integration with Jore4 can
    be implemented.

--- src/main/java/org/rutebanken/tiamat/config/OAuth2Config.java
+++ src/main/java/org/rutebanken/tiamat/config/OAuth2Config.java
@@ -49,14 +49,17 @@ public class OAuth2Config {
      *
      * @return a @{@link JwtDecoder} for Auth0.
      */
-    @Bean
-    @Profile("!test")
-    public JwtDecoder rorAuth0JwtDecoder(OAuth2ResourceServerProperties properties,
+    // TODO: replace with proper authentication
+    //@Bean
+    //@Profile("!test")
+    public JwtDecoder rorAuth0JwtDecoder(/*OAuth2ResourceServerProperties properties,*/
                                                                                              @Value("${tiamat.oauth2.resourceserver.auth0.ror.jwt.audience}") String rorAuth0Audience,
-                                         @Value("${tiamat.oauth2.resourceserver.auth0.ror.claim.namespace}") String rorAuth0ClaimNamespace) {
+                                         @Value("${tiamat.oauth2.resourceserver.auth0.ror.claim.namespace}") String rorAuth0ClaimNamespace,
+                                         @Value("${tiamat.oauth2.resourceserver.auth0.ror.issuer}") String rorAuth0Issuer
+    ) {
 
-        String rorAuth0Issuer = properties.getJwt().getIssuerUri();
-        return new RoRJwtDecoderBuilder().withIssuer(rorAuth0Issuer)
+        return new RoRJwtDecoderBuilder()
+                .withIssuer(rorAuth0Issuer)
                 .withAudience(rorAuth0Audience)
                 .withAuth0ClaimNamespace(rorAuth0ClaimNamespace)
                 .build();
