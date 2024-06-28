fffb65e755e2004c8bffc17b50abd44f4014d107
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-06-28 18:53:28 +0300

    Persist info spot WIP

--- src/main/java/org/rutebanken/tiamat/rest/graphql/types/InfoSpotObjectTypeCreator.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/types/InfoSpotObjectTypeCreator.java
@@ -10,6 +10,7 @@ import org.rutebanken.tiamat.model.PosterPlaceTypeEnumeration;
 import org.rutebanken.tiamat.model.PosterSizeEnumeration;
 import org.springframework.stereotype.Component;
 
+import static graphql.Scalars.GraphQLBigInteger;
 import static graphql.Scalars.GraphQLBoolean;
 import static graphql.Scalars.GraphQLString;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
@@ -20,9 +21,13 @@ import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.BACKLIGHT;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.DESCRIPTION;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.DISPLAY_TYPE;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.FLOOR;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.ID;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.INFO_SPOTS;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.INPUT_TYPE_INFO_SPOT;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.LABEL;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.LINES;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.MAINTENANCE;
+import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.MAXIMUM_STAY;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.ON_STOP_PLACE;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.OUTPUT_TYPE_INFO_SPOT;
 import static org.rutebanken.tiamat.rest.graphql.GraphQLNames.POSTER;
@@ -61,6 +66,23 @@ public class InfoSpotObjectTypeCreator {
                             .name(POSTER_SIZE)
                             .type(posterSizeEnum))
                     .field(newFieldDefinition()
+                            .name(LINES)
+                            .type(GraphQLString))
+                    .build();
+
+    public static GraphQLInputObjectType posterInputObjectType =
+            newInputObject()
+                    .name(POSTER)
+                    .field(newInputObjectField()
+                            .name(LABEL)
+                            .type(GraphQLString))
+                    .field(newInputObjectField()
+                            .name(POSTER_TYPE)
+                            .type(GraphQLString))
+                    .field(newInputObjectField()
+                            .name(POSTER_SIZE)
+                            .type(posterSizeEnum))
+                    .field(newInputObjectField()
                             .name(LINES)
                             .type(GraphQLString))
                     .build();
@@ -119,5 +141,66 @@ public class InfoSpotObjectTypeCreator {
                         .name(ON_STOP_PLACE)
                         .type(new GraphQLList(GraphQLString)))
                 .build();
+    }
+
+    public GraphQLInputObjectType infoSpotInputType(GraphQLInputObjectType validBetweenInputObjectType) {
+        return newInputObject()
+                .name(INFO_SPOTS)
+                .field(newInputObjectField()
+                        .name(ID)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(LABEL)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(PURPOSE)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(POSTER_PLACE_TYPE)
+                        .type(posterPlaceTypeEnum))
+                .field(newInputObjectField()
+                        .name(POSTER_PLACE_SIZE)
+                        .type(posterSizeEnum))
+                .field(newInputObjectField()
+                        .name(DESCRIPTION)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(BACKLIGHT)
+                        .type(GraphQLBoolean))
+                .field(newInputObjectField()
+                        .name(MAINTENANCE)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(ZONE_LABEL)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(RAIL_INFORMATION)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(FLOOR)
+                        .type(GraphQLString))
+                .field(newInputObjectField()
+                        .name(SPEECH_PROPERTY)
+                        .type(GraphQLBoolean))
+                .field(newInputObjectField()
+                        .name(DISPLAY_TYPE)
+                        .type(displayTypeEnum))
+                .field(newInputObjectField()
+                        .name(POSTER)
+                        .type(new GraphQLList(posterObjectType)))
+                .field(newInputObjectField()
+                        .name(ON_STOP_PLACE)
+                        .type(new GraphQLList(GraphQLString)))
+                .build();
+    }
+
+    public GraphQLInputObjectType createInputObjectType(GraphQLInputObjectType validBetweenInputObjectType) {
+        return newInputObject()
+                .name(INPUT_TYPE_INFO_SPOT)
+                .field(newInputObjectField()
+                        .name(INFO_SPOTS)
+                        .type(new GraphQLList(infoSpotInputType(validBetweenInputObjectType))))
+                .build();
+
     }
 }
