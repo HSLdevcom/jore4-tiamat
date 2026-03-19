f726bc623f5daaac50b195a99753e04460823a4e
Teemu Mäkinen <teemu.makinen@vincit.fi>
2026-03-19 14:31:28 +0200

    Use main database
    Change the used database to the shared "main" database instead of a
    database used only by Tiamat.

--- script/build-jdbc-urls.sh
+++ script/build-jdbc-urls.sh
@@ -7,5 +7,5 @@ set -eu
 # if TIAMAT_DB_URL was not set, build it from parts
 if [ -z "${TIAMAT_DB_URL+x}" ]; then
   export TIAMAT_DB_PORT=${TIAMAT_DB_PORT:-"5432"}
-  export TIAMAT_DB_URL="jdbc:postgresql://${TIAMAT_DB_HOSTNAME}:${TIAMAT_DB_PORT}/${TIAMAT_DB_DATABASE}?stringtype=unspecified"
+  export TIAMAT_DB_URL="jdbc:postgresql://${TIAMAT_DB_HOSTNAME}:${TIAMAT_DB_PORT}/${TIAMAT_DB_MAIN_DATABASE}?stringtype=unspecified"
 fi
